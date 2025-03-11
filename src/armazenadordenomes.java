cbd3ff7198f4666b5275717d54cd0b1d616c3222
Webermanoel <webermanoel22@gmail.com>
2025-03-10 22:09:46 -0400

    novaestrutura

--- src/armazenadordenomes.java
+++ src/armazenadordenomes.java
@@ -4,9 +4,9 @@ import java.util.LinkedList;
 
 public class armazenadordenomes {
     public static void main(String[] args) {
-        System.out.println("###################################################");
-        System.out.println("Bem-vindo ao armazenador de nomes do banco de dados");
-        System.out.println("###################################################\n");
+        System.out.println("################################");
+        System.out.println("Bem-vindo ao banco de dados java");
+        System.out.println("################################\n");
 
         Scanner scanner = new Scanner(System.in);
         LinkedList<String> list = new LinkedList<>();
@@ -20,10 +20,33 @@ public class armazenadordenomes {
             list.add(input);
         }
 
-        System.out.println("Nomes armazenados: " + list);
+        Scanner number = new Scanner(System.in);
+        LinkedList<String> listage= new LinkedList<>();
+
+        System.out.print("Digite as idades que deseja armazenar (ou 'exit' para sair): ");
+        while (true) {
+            String input = number.nextLine();
+
+            if (input.equalsIgnoreCase("exit")) {
+                break;
+            }
+
+            try {
+                int age = Integer.parseInt(input);
+                listage.add(String.valueOf(age));
+            } catch (NumberFormatException e) {
+                System.out.println("Entrada inválida! Digite um número ou 'exit' para sair.");
+            }
+        }
+
+        System.out.println("Os nomes e idades armazenados são nomes: " + list
+        + " idades: " + listage);
     }
 
 
+}
 
-    }
+
+
+
 
